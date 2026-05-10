1845b773cbc6f9865be5fe83ee79a41bda315099
Jhosep Argomedo <jhosepjamil@gmail.com>
2026-05-10 15:15:49 -0500

    feat(profile): add profile interfaces/

---
+++ IoBuilt.API/Profiles/Interfaces/REST/Transform/ProfileResourceFromEntityAssembler.cs
@@ -0,0 +1,21 @@
+using IoBuilt.API.Profiles.Domain.Model.Aggregates;
+using IoBuilt.API.Profiles.Interfaces.REST.Resources;
+
+namespace IoBuilt.API.Profiles.Interfaces.REST.Transform;
+
+public static class ProfileResourceFromEntityAssembler
+{
+    public static ProfileResource ToResourceFromEntity(Profile entity)
+    {
+        return new ProfileResource(
+            entity.Id, 
+            entity.UserId, 
+            entity.PhotoUrl, 
+            entity.Name, 
+            entity.Username, 
+            entity.Address, 
+            entity.Age, 
+            entity.PhoneNumber,
+            entity.SecondEmail);
+    }
+}
